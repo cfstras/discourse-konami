14cfd5fdf26802a43884abb2c6a1391852fc8815
Claus Strasburger <claus@strasburger.de>
2014-01-31 17:00:22 +0100

    update for new plugin system

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: Konami
+# about: Every website should have the Konami code
+# version: 0.1
+# authors: Claus Strasburger
+
+register_asset "javascripts/konami.js"
+register_asset "javascripts/discourse_konami.js"
